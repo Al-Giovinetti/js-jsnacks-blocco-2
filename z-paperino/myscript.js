917438319e54c941f29f03c439424bbbe78cd4b5
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-04 16:55:10 +0200

    ex-rifatto

--- z-paperino/myscript.js
+++ z-paperino/myscript.js
@@ -1,2 +1,9 @@
+const randomCaseList = ["PiPPO","pluTo","PAPErino","pluTONio","PAperMaTe"]
 
+const orderCaseList = randomCaseList.map((character) =>{
+   let haracter = character.substring(1);
+    return character.charAt(0).toUpperCase()+haracter.toLowerCase()
+})
+
+console.log(orderCaseList)
 
