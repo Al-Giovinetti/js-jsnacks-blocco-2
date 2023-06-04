78790dc39c4db59c78a1d15d4591b17016781fe4
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-04 10:44:33 +0200

    ex-3 sistemato

--- ex-3/syscript.js
+++ ex-3/syscript.js
@@ -1,17 +1,11 @@
 //calcola la media dei 10 numeri
-const userInput = document.querySelector("input")
-const btnCalc = document.querySelector("button")
 
 let sum = 0
-btnCalc.addEventListener("click",function(){
-    let userNumber;
-    for( let i = 0; i < 10; i++){
-        userNumber = userInput.value
-        sum = sum + userNumber
-        console.log(userNumber)
-        userNumber= ""
-    }
-    const media = (sum / 10).toFixed(2)
-    console.log(media)
+let userNumber;
+for( let i = 0; i < 10; i++){
+    userNumber = parseInt(prompt("dammi un numero"))
+    sum += userNumber
 }
-);
+const media = (sum / 10).toFixed(2)
+console.log(media)
+
